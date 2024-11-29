b885305dab1617ae4ccf35dc6fefe67d9276bedc
Hezzie <hezronchelimo.hc@gmail.com>
2024-11-29 17:29:24 +0300

    setup folder structure,  added AI directories

---
+++ math/math.go
@@ -0,0 +1,19 @@
+package math
+
+func Add(x, y int) (res int) {
+	return x + y
+}
+
+func Subtract(x, y int) (res int) {
+	return x - y
+}
+func Multiply(x, y int) (res int) {
+	return x * y
+}
+
+func Divide(x, y int) (res int) {
+	return x / y
+}
+func Modulus(x, y int) (res int) {
+	return x % y
+}
